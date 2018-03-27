a53927b11909ca4dd94850e74251a21cf4a31040
KillerCode88 <36702442+KillerCode88@users.noreply.github.com>
2018-03-27 10:39:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,114 +1,157 @@
 <?php
-class LandVehicle {
+
+class LandVehicle
+{
     protected $color;
     protected $wheelCount;
+
     public function getColor()
     {
         return $this->color;
     }
+
     public function setColor($color)
     {
         $this->color = $color;
     }
+
     public function getWheelCount()
     {
         return $this->wheelCount;
     }
+
     public function setWheelCount($wheelCount)
     {
         $this->wheelCount = $wheelCount;
     }
 }
+
 interface Drivable
 {
     public function startEngine();
+
     public function stopEngine();
+
     public function drive();
+
     public function brake();
 }
-class Car extends LandVehicle implements Drivable {
+
+class Car extends LandVehicle implements Drivable
+{
     protected $color = 'white';
     protected $wheelCount = 4;
     private $seats = 5;
+
     public function getSeats()
     {
         return $this->seats;
     }
+
     public function startEngine()
     {
         echo 'Двигатель запущен';
     }
+
     public function stopEngine()
     {
         echo 'Двигатель заглушен';
     }
+
     public function drive()
     {
         echo 'Поехали';
     }
+
     public function brake()
     {
         echo 'Стоп';
     }
 }
+
 $car1 = new Car;
 $car1->setColor('green');
 $car2 = new Car;
 $car2->getSeats();
-class HomeAppliance {
+
+class HomeAppliance
+{
     protected $price;
+
     public function getPrice()
     {
         return $this->price;
     }
+
     public function setPrice($price)
     {
         $this->price = $price;
     }
 }
-interface Watchable {
+
+interface Watchable
+{
     public function turnOn();
+
     public function turnOff();
+
     public function watch();
 }
-class TV  extends HomeAppliance implements Watchable {
+
+class TV extends HomeAppliance implements Watchable
+{
     private $screenSize;
     private $isSmart;
-    public function __construct($screenSize, $isSmart, $price) {
+
+    public function __construct($screenSize, $isSmart, $price)
+    {
         $this->screenSize = $screenSize;
         $this->isSmart = $isSmart;
         $this->price = $price;
     }
+
     public function turnOn()
     {
         echo 'Телевизор включен';
     }
+
     public function turnOff()
     {
         echo 'Телевизор выключен';
     }
+
     public function watch()
     {
         echo 'Смотрим';
     }
 }
+
 $tv1 = new TV(40, true, 10);
 $tv1->setPrice(5);
 $tv2 = new TV(50, false, 7);
 $tv2->setPrice(5);
-class WritingUtensil {
+
+class WritingUtensil
+{
     protected $color;
     protected $capacity;
 }
-interface Writable {
+
+interface Writable
+{
     public function write();
 }
-class Pen extends WritingUtensil implements Writable {
-    public function refill($newColor) {
+
+class Pen extends WritingUtensil implements Writable
+{
+    public function refill($newColor)
+    {
         $this->color = $newColor;
         $this->capacity = 10;
     }
-    public function write() {
+
+    public function write()
+    {
         if ($this->capacity === 0) {
             echo 'У вас закончились чернила';
             exit;
@@ -116,29 +159,36 @@ class Pen extends WritingUtensil implements Writable {
         $this->capacity--;
     }
 }
+
 $pen1 = new Pen;
 $pen1->refill('black');
 $pen2 = new Pen;
 $pen2->write();
 $pen2->refill('red');
 
-abstract class Goods {
+abstract class Goods
+{
     protected $name;
     protected $price;
     protected $weight;
     protected $discount = 0;
     protected $delivery = 250;
+
     public function __construct($price, $weight)
     {
         $this->price = $price;
         $this->weight = $weight;
     }
+
     abstract function setName($name);
+
     protected function setDiscount()
     {
         $this->discount = 0;
     }
-    protected function getPrice() {
+
+    protected function getPrice()
+    {
         if ($this->discount !== 0) {
             $this->delivery = 300;
         }
@@ -146,7 +196,9 @@ abstract class Goods {
         return $newPrice;
     }
 }
-class Goods1 extends Goods {
+
+class Goods1 extends Goods
+{
     public function setName($name)
     {
         $this->name = $name;
@@ -154,7 +206,9 @@ class Goods1 extends Goods {
         return $this;
     }
 }
-class Goods2 extends Goods {
+
+class Goods2 extends Goods
+{
     public function setName($name)
     {
         $this->name = $name;
@@ -162,13 +216,16 @@ class Goods2 extends Goods {
         return $this;
     }
 }
-class Goods3 extends Goods {
+
+class Goods3 extends Goods
+{
     public function setName($name)
     {
         $this->name = $name;
         echo 'Продукт 2: ' . $name;
         return $this;
     }
+
     public function setDiscount()
     {
         if ($this->weight > 10) {
